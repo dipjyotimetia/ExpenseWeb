692541fca4058eb333b6e0e0c9c26a3b505ff18e
Dipjyoti Metia <dipjyotimetia@gmail.com>
2020-05-05 12:10:09 +1000

    added sentry support

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from '@sentry/browser';
 
 import App from "./App";
+
+Sentry.init({dsn: "https://98316c0e57574ee7afdfca9dc6f29f67@o263555.ingest.sentry.io/5223763"});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);

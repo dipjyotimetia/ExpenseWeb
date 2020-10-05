afd28a783a23d330b9969dbbceea9a8cb4d1a6b5
Dipjyoti Metia <dipjyotimetia@gmail.com>
2020-10-06 00:01:44 +1100

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as Sentry from '@sentry/browser';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
 import App from "./App";
 
-Sentry.init({dsn: "https://98316c0e57574ee7afdfca9dc6f29f67@o263555.ingest.sentry.io/5223763"});
+Sentry.init({
+    dsn: "https://98316c0e57574ee7afdfca9dc6f29f67@o263555.ingest.sentry.io/5223763",
+    integrations: [
+      new Integrations.BrowserTracing(),
+    ],
+    tracesSampleRate: 1.0,
+  });
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
